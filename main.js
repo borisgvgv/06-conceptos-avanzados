81ffa6a932713acc5252b370cc3ae7c37b019b58
Boris Garcés <borisgarces@imac-de-boris.home>
2023-08-30 15:32:18 +0200

    Act all

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
+import { environmentsComponents } from './src/concepts/01-environments';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,10 +12,16 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
+    
     <div class="card">
  
     </div>
     
   </div>
 `;
-console.log('Hola mundo');
+
+// Ref HTML
+const idElementCard = document.querySelector('.card');
+
+// Llamadas
+environmentsComponents(idElementCard);
