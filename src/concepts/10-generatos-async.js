1d2f16dde697cbadd8ce2ac89f06c11ea3ce947d
Boris Garcés <borisgarces@imac-de-boris.home>
2023-09-03 14:45:54 +0200

    Act All

--- src/concepts/10-generatos-async.js
+++ src/concepts/10-generatos-async.js
@@ -1,9 +1,33 @@
+import { heroes } from '../data/heroes';
 // environmentsComponents recibe el elementoId del htm donde quiero renderizar lo que necesite
 /**
  *
  * @param {HTMLdivElement} idElementCard
  */
-export const generatorAsyncComponent = (idElementCard) => {
-  console.log('generatorAsyncComponent');
-  return; // detiene la ejecución de la función y regresa undefined
+export const generatorAsyncComponent = async (idElementCard) => {
+  const heroeGenerator = getHeroeGenerator();
+  let isFinished = false;
+  do {
+    const { value, done } = await heroeGenerator.next();
+    isFinished = done;
+    if (isFinished) break;
+    idElementCard.innerHTML = value;
+  } while (!isFinished);
+};
+
+async function* getHeroeGenerator() {
+  for (const heroe of heroes) {
+    //await sleep() hace que demore 1s en disparar cada unaa de las emiciones que le mandemos con el yield
+    await sleep();
+    yield heroe.name;
+  }
+  return 'Ya no hay más heroes';
+}
+
+const sleep = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 500);
+  });
 };
